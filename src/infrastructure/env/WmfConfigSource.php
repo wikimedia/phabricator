b88502e7c3f299ff95ff21613ca1d53b26fa6438
Mukunda Modell <mmodell@wikimedia.org>
2016-11-18 10:39:02 -0600

    WmfConfigSource: Attempt to load a readable config file in all cases
    Maniphest Tasks: T146055, T151059
    
    Summary:
    Setting PHABRICATOR_ENV is fragile - there are a bunch of edge cases
    where it isn't propogated to sub/remote processes.
    
    Figuring out which file is readable and loading that as a fallback
    actually makes sense because of the way we have divided privileges to
    those config files - only one of the files should be owned by the same
    gid as the active process so we use that one if the environement var
    isn't set.
    
    refs T146055
    fixes T151059
    
    Test Plan: tested locally for basic sanity. Unfortunately it is currently difficult to test this part of phabricator adequately.
    
    Reviewers: chasemp, Krenair, Dzahn
    
    Differential Revision: https://phabricator.wikimedia.org/D460

--- src/infrastructure/env/WmfConfigSource.php
+++ src/infrastructure/env/WmfConfigSource.php
@@ -6,7 +6,11 @@
  */
 final class WmfConfigSource extends PhabricatorConfigSiteSource {
 
+  private $root = null;
+  private $readableConfigFiles = null;
+
   public function __construct() {
+    $this->root = dirname(phutil_get_library_root('phabricator'));
     $config = $this->loadConfig();
     $this->setSource(new PhabricatorConfigDictionarySource($config));
   }
@@ -45,10 +49,68 @@ final class WmfConfigSource extends PhabricatorConfigSiteSource {
     Filesystem::writeFile($this->getConfigPath(), $data);
   }
 
+  /**
+  * get a list of config files which are owned by
+  * the same gid as the current process.
+  */
+  private function getReadableConfigFiles() {
+    if ($this->readableConfigFiles != null) {
+      return $this->readableConfigFiles;
+    }
+    $userinfo = posix_getpwuid(posix_geteuid());
+    $gid = $userinfo['gid'];
+    $results = array();
+    $files = glob("$this->root/conf/local/*.json");
+    foreach ($files as $filename) {
+      if (substr($filename, -10) !== 'local.json' &&
+          filegroup($filename) == $gid) {
+        $results[] = $filename;
+      }
+    }
+    return $this->readableConfigFiles = $results;
+  }
+
+  /** get the path to a config file for the selected environment,
+   * falling back to any readable config file or the alternate
+   * environment specified by $default
+   */
+  private function getReadablePathForEnv($env, $default = false) {
+    $path = "$this->root/conf/local/$env.json";
+    if (strlen($env) && file_exists($path) && is_readable($path)) {
+      return $path;
+    }
+
+    $files = $this->getReadableConfigFiles();
+    if (count($files)) {
+      return array_pop($files);
+    }
+
+    if ($default) {
+      $path = "$this->root/conf/local/$default.json";
+      if (file_exists($path)) {
+        return $path;
+      }
+    }
+    return false;
+  }
+
+  /** get the config file for the current environment, if no environment is set,
+   * return any readable config file. If all else fails, just return 'phd.json'
+   */
   private function getConfigPath() {
-    $root = dirname(phutil_get_library_root('phabricator'));
-    $env = PhabricatorEnv::getSelectedEnvironmentName();
-    $path = "$root/conf/local/$env.json";
+    $environment = PhabricatorEnv::getSelectedEnvironmentName();
+    if (isset($_ENV['PHABRICATOR_INSTANCE'])) {
+      $default = $environment;
+      $environment = $_ENV['PHABRICATOR_INSTANCE'];
+    } else {
+      $default = 'phd';
+    }
+
+    $path = $this->getReadablePathForEnv($environment, $default);
+
+    $_ENV['PHABRICATOR_INSTANCE'] = $environment;
+    putenv("PHABRICATOR_INSTANCE=$environment");
+
     return $path;
   }
 
