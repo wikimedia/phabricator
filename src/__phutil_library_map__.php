wikimedia/phabricator

commit 3245e74f16bb28ae905f76fcc99d722abc8320ab

Show users how fulltext search queries are parsed and executed; don't query stopwords or short tokens
Summary:
Depends on D17670. Fixes T12137. Fixes T12003. Ref T2632.

This shows users a readout of which terms were actually searched for.

This also drops those terms from the query we submit to the backend, dodging the weird behaviors / search engine bugs in T12137.

This might need some design tweaking.

Test Plan: {F4899825}

Reviewers: chad

Reviewed By: chad

Maniphest Tasks: T12137, T12003, T2632

Differential Revision: https://secure.phabricator.com/D17672

src/__phutil_library_map__.php
@@ -2844,7 +2844,9 @@ phutil_register_library_map(array(
     'PhabricatorFulltextEngineExtensionModule' => 'applications/search/index/PhabricatorFulltextEngineExtensionModule.php',
     'PhabricatorFulltextIndexEngineExtension' => 'applications/search/engineextension/PhabricatorFulltextIndexEngineExtension.php',
     'PhabricatorFulltextInterface' => 'applications/search/interface/PhabricatorFulltextInterface.php',
+    'PhabricatorFulltextResultSet' => 'applications/search/query/PhabricatorFulltextResultSet.php',
     'PhabricatorFulltextStorageEngine' => 'applications/search/fulltextstorage/PhabricatorFulltextStorageEngine.php',
+    'PhabricatorFulltextToken' => 'applications/search/query/PhabricatorFulltextToken.php',
     'PhabricatorFundApplication' => 'applications/fund/application/PhabricatorFundApplication.php',
     'PhabricatorGDSetupCheck' => 'applications/config/check/PhabricatorGDSetupCheck.php',
     'PhabricatorGarbageCollector' => 'infrastructure/daemon/garbagecollector/PhabricatorGarbageCollector.php',
@@ -8005,7 +8007,9 @@ phutil_register_library_map(array(
     'PhabricatorFulltextEngineExtension' => 'Phobject',
     'PhabricatorFulltextEngineExtensionModule' => 'PhabricatorConfigModule',
     'PhabricatorFulltextIndexEngineExtension' => 'PhabricatorIndexEngineExtension',
+    'PhabricatorFulltextResultSet' => 'Phobject',
     'PhabricatorFulltextStorageEngine' => 'Phobject',
+    'PhabricatorFulltextToken' => 'Phobject',
     'PhabricatorFundApplication' => 'PhabricatorApplication',
     'PhabricatorGDSetupCheck' => 'PhabricatorSetupCheck',
     'PhabricatorGarbageCollector' => 'Phobject',

src/applications/search/fulltextstorage/PhabricatorMySQLFulltextStorageEngine.php
@@ -3,6 +3,9 @@
 final class PhabricatorMySQLFulltextStorageEngine
   extends PhabricatorFulltextStorageEngine {
 
+  private $fulltextTokens = array();
+  private $engineLimits;
+
   public function getEngineIdentifier() {
     return 'mysql';
   }
@@ -203,8 +206,64 @@ final class PhabricatorMySQLFulltextStorageEngine
     $title_field = PhabricatorSearchDocumentFieldType::FIELD_TITLE;
     $title_boost = 1024;
 
+    $stemmer = new PhutilSearchStemmer();
+
     $raw_query = $query->getParameter('query');
-    $compiled_query = $this->compileQuery($raw_query);
+    $raw_query = trim($raw_query);
+    if (strlen($raw_query)) {
+      $compiler = PhabricatorSearchDocument::newQueryCompiler()
+        ->setStemmer($stemmer);
+
+      $tokens = $compiler->newTokens($raw_query);
+
+      list($min_length, $stopword_list) = $this->getEngineLimits($conn);
+
+      // Process all the parts of the user's query so we can show them which
+      // parts we searched for and which ones we ignored.
+      $fulltext_tokens = array();
+      foreach ($tokens as $key => $token) {
+        $fulltext_token = id(new PhabricatorFulltextToken())
+          ->setToken($token);
+
+        $fulltext_tokens[$key] = $fulltext_token;
+
+        $value = $token->getValue();
+        if (phutil_utf8_strlen($value) < $min_length) {
+          $fulltext_token->setIsShort(true);
+          continue;
+        }
+
+        if (isset($stopword_list[phutil_utf8_strtolower($value)])) {
+          $fulltext_token->setIsStopword(true);
+          continue;
+        }
+      }
+      $this->fulltextTokens = $fulltext_tokens;
+
+      // Remove tokens which aren't queryable from the query. This is mostly
+      // a workaround for the peculiar behaviors described in T12137.
+      foreach ($this->fulltextTokens as $key => $fulltext_token) {
+        if (!$fulltext_token->isQueryable()) {
+          unset($tokens[$key]);
+        }
+      }
+
+      if (!$tokens) {
+        throw new PhutilSearchQueryCompilerSyntaxException(
+          pht(
+            'All of your search terms are too short or too common to '.
+            'appear in the search index. Search for longer or more '.
+            'distinctive terms.'));
+      }
+
+      $queries = array();
+      $queries[] = $compiler->compileLiteralQuery($tokens);
+      $queries[] = $compiler->compileStemmedQuery($tokens);
+      $compiled_query = implode(' ', array_filter($queries));
+    } else {
+      $compiled_query = null;
+    }
+
     if (strlen($compiled_query)) {
       $select[] = qsprintf(
         $conn,
@@ -394,27 +453,64 @@ final class PhabricatorMySQLFulltextStorageEngine
     return $sql;
   }
 
-  private function compileQuery($raw_query) {
-    $stemmer = new PhutilSearchStemmer();
-
-    $compiler = PhabricatorSearchDocument::newQueryCompiler()
-      ->setStemmer($stemmer);
-
-    $tokens = $compiler->newTokens($raw_query);
-
-    $queries = array();
-    $queries[] = $compiler->compileLiteralQuery($tokens);
-    $queries[] = $compiler->compileStemmedQuery($tokens);
-
-    return implode(' ', array_filter($queries));
-  }
-
   public function indexExists() {
     return true;
   }
 
   public function getIndexStats() {
     return false;
+  }
+
+  public function getFulltextTokens() {
+    return $this->fulltextTokens;
+  }
+
+  private function getEngineLimits(AphrontDatabaseConnection $conn) {
+    if ($this->engineLimits === null) {
+      $this->engineLimits = $this->newEngineLimits($conn);
+    }
+    return $this->engineLimits;
+  }
+
+  private function newEngineLimits(AphrontDatabaseConnection $conn) {
+    $result = queryfx_one(
+      $conn,
+      'SELECT
+        @@innodb_ft_min_token_size innodb_max,
+        @@ft_min_word_len myisam_max,
+        @@ft_stopword_file myisam_stopwords');
+
+    if ($result['innodb_max']) {
+      $min_len = $result['innodb_max'];
+      $stopwords = queryfx_all(
+        $conn,
+        'SELECT * FROM INFORMATION_SCHEMA.INNODB_FT_DEFAULT_STOPWORD');
+      $stopwords = ipull($stopwords, 'value');
+      $stopwords = array_fuse($stopwords);
+    } else {
+      $min_len = $result['myisam_max'];
+
+      $file = $result['myisam_stopwords'];
+      if (preg_match('(/resources/sql/stopwords\.txt\z)', $file)) {
+        // If this is set to something that looks like the Phabricator
+        // stopword file, read that.
+        $file = 'stopwords.txt';
+      } else {
+        // Otherwise, just use the default stopwords. This might be wrong
+        // but we can't read the actual value dynamically and reading
+        // whatever file the variable is set to could be a big headache
+        // to get right from a security perspective.
+        $file = 'stopwords_myisam.txt';
+      }
+
+      $root = dirname(phutil_get_library_root('phabricator'));
+      $data = Filesystem::readFile($root.'/resources/sql/'.$file);
+      $stopwords = explode("\n", $data);
+      $stopwords = array_filter($stopwords);
+      $stopwords = array_fuse($stopwords);
+    }
+
+    return array($min_len, $stopwords);
   }
 
 }

src/applications/search/query/PhabricatorFulltextResultSet.php
@@ -1,0 +1,26 @@
+<?php
+
+final class PhabricatorFulltextResultSet extends Phobject {
+
+  private $phids;
+  private $fulltextTokens;
+
+  public function setPHIDs($phids) {
+    $this->phids = $phids;
+    return $this;
+  }
+
+  public function getPHIDs() {
+    return $this->phids;
+  }
+
+  public function setFulltextTokens($fulltext_tokens) {
+    $this->fulltextTokens = $fulltext_tokens;
+    return $this;
+  }
+
+  public function getFulltextTokens() {
+    return $this->fulltextTokens;
+  }
+
+}

src/applications/search/query/PhabricatorSearchApplicationSearchEngine.php
@@ -3,6 +3,8 @@
 final class PhabricatorSearchApplicationSearchEngine
   extends PhabricatorApplicationSearchEngine {
 
+  private $resultSet;
+
   public function getResultTypeDescription() {
     return pht('Fulltext Search Results');
   }
@@ -243,6 +245,9 @@ final class PhabricatorSearchApplicationSearchEngine
     PhabricatorSavedQuery $query,
     array $handles) {
 
+    $result_set = $this->resultSet;
+    $fulltext_tokens = $result_set->getFulltextTokens();
+
     $viewer = $this->requireViewer();
     $list = new PHUIObjectItemListView();
     $list->setNoDataString(pht('No results found.'));
@@ -263,7 +268,28 @@ final class PhabricatorSearchApplicationSearchEngine
       }
     }
 
+    $fulltext_view = null;
+    if ($fulltext_tokens) {
+      require_celerity_resource('phabricator-search-results-css');
+
+      $fulltext_view = array();
+      foreach ($fulltext_tokens as $token) {
+        $fulltext_view[] = $token->newTag();
+      }
+      $fulltext_view = phutil_tag(
+        'div',
+        array(
+          'class' => 'phui-fulltext-tokens',
+        ),
+        array(
+          pht('Searched For:'),
+          ' ',
+          $fulltext_view,
+        ));
+    }
+
     $result = new PhabricatorApplicationSearchResultView();
+    $result->setContent($fulltext_view);
     $result->setObjectList($list);
 
     return $result;
@@ -278,6 +304,10 @@ final class PhabricatorSearchApplicationSearchEngine
     }
 
     return $owner_phids;
+  }
+
+  protected function didExecuteQuery(PhabricatorPolicyAwareQuery $query) {
+    $this->resultSet = $query->getFulltextResultSet();
   }
 
 }

src/applications/search/query/PhabricatorSearchDocumentQuery.php
@@ -6,6 +6,7 @@ final class PhabricatorSearchDocumentQuery
   private $savedQuery;
   private $objectCapabilities;
   private $unfilteredOffset;
+  private $fulltextResultSet;
 
   public function withSavedQuery(PhabricatorSavedQuery $query) {
     $this->savedQuery = $query;
@@ -25,8 +26,17 @@ final class PhabricatorSearchDocumentQuery
     return $this->getRequiredCapabilities();
   }
 
+  public function getFulltextResultSet() {
+    if (!$this->fulltextResultSet) {
+      throw new PhutilInvalidStateException('execute');
+    }
+
+    return $this->fulltextResultSet;
+  }
+
   protected function willExecute() {
     $this->unfilteredOffset = 0;
+    $this->fulltextResultSet = null;
   }
 
   protected function loadPage() {
@@ -39,8 +49,10 @@ final class PhabricatorSearchDocumentQuery
       ->setParameter('offset', $this->unfilteredOffset)
       ->setParameter('limit', $this->getRawResultLimit());
 
-    $phids = PhabricatorSearchService::executeSearch($query);
+    $result_set = PhabricatorSearchService::newResultSet($query, $this);
+    $phids = $result_set->getPHIDs();
 
+    $this->fulltextResultSet = $result_set;
     $this->unfilteredOffset += count($phids);
 
     $handles = id(new PhabricatorHandleQuery())

src/infrastructure/cluster/search/PhabricatorSearchService.php
@@ -245,14 +245,25 @@ class PhabricatorSearchService
    * @throws PhutilAggregateException
    */
   public static function executeSearch(PhabricatorSavedQuery $query) {
+    $result_set = self::newResultSet($query);
+    return $result_set->getPHIDs();
+  }
+
+  public static function newResultSet(PhabricatorSavedQuery $query) {
     $exceptions = array();
     // try all services until one succeeds
     foreach (self::getAllServices() as $service) {
+      if (!$service->isReadable()) {
+        continue;
+      }
+
       try {
         $engine = $service->getEngine();
-        $res = $engine->executeSearch($query);
-        // return immediately if we get results
-        return $res;
+        $phids = $engine->executeSearch($query);
+
+        return id(new PhabricatorFulltextResultSet())
+          ->setPHIDs($phids)
+          ->setFulltextTokens($engine->getFulltextTokens());
       } catch (PhutilSearchQueryCompilerSyntaxException $ex) {
         // If there's a query compilation error, return it directly to the
         // user: they issued a query with bad syntax.

resources/celerity/map.php
@@ -103,7 +103,7 @@ return array(
     'rsrc/css/application/releeph/releeph-request-differential-create-dialog.css' => '8d8b92cd',
     'rsrc/css/application/releeph/releeph-request-typeahead.css' => '667a48ae',
     'rsrc/css/application/search/application-search-view.css' => '66ee5d46',
-    'rsrc/css/application/search/search-results.css' => '64ad079a',
+    'rsrc/css/application/search/search-results.css' => 'f87d23ad',
     'rsrc/css/application/slowvote/slowvote.css' => 'a94b7230',
     'rsrc/css/application/tokens/tokens.css' => '3d0f239e',
     'rsrc/css/application/uiexample/example.css' => '528b19de',
@@ -794,7 +794,7 @@ return array(
     'phabricator-phtize' => 'd254d646',
     'phabricator-prefab' => '8d40ae75',
     'phabricator-remarkup-css' => '17c0fb37',
-    'phabricator-search-results-css' => '64ad079a',
+    'phabricator-search-results-css' => 'f87d23ad',
     'phabricator-shaped-request' => '7cbe244b',
     'phabricator-slowvote-css' => 'a94b7230',
     'phabricator-source-code-view-css' => '4383192f',

src/applications/search/engine/PhabricatorApplicationSearchEngine.php
@@ -978,7 +978,13 @@ abstract class PhabricatorApplicationSearchEngine extends Phobject {
       $objects = $query->executeWithCursorPager($pager);
     }
 
+    $this->didExecuteQuery($query);
+
     return $objects;
+  }
+
+  protected function didExecuteQuery(PhabricatorPolicyAwareQuery $query) {
+    return;
   }
 
 

commit 656366f289d36d3ce181f0db6511703f1f71c5a3

Make search highlights work with upstream changes
Upstream changes broke search highlighting in 3245e74f, this adapts
the PhabricatorSearchResultEngineExtension to work with those changes.

src/__phutil_library_map__.php
@@ -2848,7 +2848,6 @@ phutil_register_library_map(array(
     'PhabricatorFulltextEngineExtensionModule' => 'applications/search/index/PhabricatorFulltextEngineExtensionModule.php',
     'PhabricatorFulltextIndexEngineExtension' => 'applications/search/engineextension/PhabricatorFulltextIndexEngineExtension.php',
     'PhabricatorFulltextInterface' => 'applications/search/interface/PhabricatorFulltextInterface.php',
-    'PhabricatorFulltextResult' => 'applications/search/fulltextstorage/PhabricatorFulltextResult.php',
     'PhabricatorFulltextResultSet' => 'applications/search/query/PhabricatorFulltextResultSet.php',
     'PhabricatorFulltextStorageEngine' => 'applications/search/fulltextstorage/PhabricatorFulltextStorageEngine.php',
     'PhabricatorFulltextToken' => 'applications/search/query/PhabricatorFulltextToken.php',
@@ -8022,7 +8021,6 @@ phutil_register_library_map(array(
     'PhabricatorFulltextEngineExtensionModule' => 'PhabricatorConfigModule',
     'PhabricatorFulltextIndexEngineExtension' => 'PhabricatorIndexEngineExtension',
     'PhabricatorFulltextInterface' => 'PhabricatorIndexableInterface',
-    'PhabricatorFulltextResult' => 'PhabricatorPolicyInterface',
     'PhabricatorFulltextResultSet' => 'Phobject',
     'PhabricatorFulltextStorageEngine' => 'Phobject',
     'PhabricatorFulltextToken' => 'Phobject',

src/applications/search/engineextension/PhabricatorSearchResultEngineExtension.php
@@ -21,9 +21,7 @@ abstract class PhabricatorSearchResultEngineExtension extends Phobject {
 
   abstract public function getExtensionName();
 
-  abstract public function canRenderItemView(PhabricatorFulltextResult $result);
-
-  public function getExtensionOrder() {
+    public function getExtensionOrder() {
     return 5000;
   }
 
@@ -32,9 +30,9 @@ abstract class PhabricatorSearchResultEngineExtension extends Phobject {
   }
 
   abstract public function renderItemView(
+    PhabricatorFulltextResultSet $result_set,
     PHUIObjectItemView $item,
-    PhabricatorFulltextResult $result);
-
+    $phid);
 
   final public static function getAllExtensions() {
     return id(new PhutilClassMapQuery())

src/applications/search/engineextension/PhabricatorSearchResultHighlightsEngineExtension.php
@@ -13,17 +13,17 @@ class PhabricatorSearchResultHighlightsEngineExtension
     return 'SearchResultHighlights';
   }
 
-  public function canRenderItemView(PhabricatorFulltextResult $result) {
-    return true;
-  }
-
   public function renderItemView(
-    PHUIObjectItemView $item,
-    PhabricatorFulltextResult $result) {
+    PhabricatorFulltextResultSet $result_set, PHUIObjectItemView $item,
+      $phid) {
 
-    $highlights = $result->getHighlights('body');
+    $highlights = $result_set->getHighlightsForPHID($phid);
     if ($highlights) {
-      $parts = explode('||SPLIT||', $highlights);
+      $flattened = "";
+      foreach($highlights as $field => $values) {
+        $flattened .= implode(' ', array_values($values));
+      }
+      $parts = explode('||SPLIT||', $flattened);
       $highlights = array();
       foreach ($parts as $part) {
         if ($part == '##STARTHIGHLIGHT##') {

src/applications/search/fulltextstorage/PhabricatorElasticFulltextStorageEngine.php
@@ -310,16 +310,21 @@ class PhabricatorElasticFulltextStorageEngine
         $response = $this->executeRequest($host, $uri, $spec);
 
         $hits = array();
+        $highlights = array();
+
         foreach ($response['hits']['hits'] as $hit) {
           $phid = $hit['_id'];
-          $result = new PhabricatorFulltextResult($phid);
           if (isset($hit['highlight'])) {
-            $result->setHighlights($hit['highlight']);
+            $highlights[$phid] = $hit['highlight'];
           }
-          $hits[$phid] = $result;
+          $phids[] = $phid;
         }
-        //phlog($hits);
-        return $hits;
+
+        return id(new PhabricatorFulltextResultSet())
+          ->setPHIDs($phids)
+          ->setFulltextTokens($this->getFulltextTokens())
+          ->setFulltextHighlights($highlights);
+
       } catch (Exception $e) {
         $exceptions[] = $e;
       }

src/applications/search/fulltextstorage/PhabricatorFulltextResult.php
@@ -1,73 +1,0 @@
-<?php
-class PhabricatorFulltextResult
-  implements PhabricatorPolicyInterface {
-
-  protected $handle;
-  protected $fields = array();
-  protected $phid;
-  protected $type;
-
-  public function __construct($phid) {
-    $this->setPHID($phid);
-  }
-
-  public function setPHID($phid) {
-    $this->phid = $phid;
-    $this->type = phid_get_type($phid);
-    return $this;
-  }
-
-  public function getPHID() {
-    return $this->phid;
-  }
-
-  public function getType() {
-    return $this->type;
-  }
-
-  /**
-   * @return PhabricatorObjectHandle
-   */
-  public function getHandle() {
-    return $this->handle;
-  }
-
-  public function setHandle($handle) {
-    $this->handle = $handle;
-    return $this;
-  }
-
-  public function setHighlights($highlights) {
-    if (!$highlights) {
-      return;
-    }
-
-    foreach ($highlights as $field => $values) {
-      $this->fields[$field] = join(' ... ', array_slice($values,0,2));
-    }
-  }
-
-  public function getHighlights($field='all') {
-    if ($field == 'all') {
-      return phutil_safe_html(join('<br/>', $this->fields));
-    } else if (!isset($this->fields[$field])) {
-      return '';
-    }
-    return phutil_safe_html($this->fields[$field]);
-  }
-
-/* -(  PhabricatorPolicyInterface  )----------------------------------------- */
-
-  public function getCapabilities() {
-    return $this->handle->getCapabilities();
-  }
-
-  public function getPolicy($capability) {
-    return $this->handle->getPolicy($capability);
-  }
-
-  public function hasAutomaticCapability($capability, PhabricatorUser $viewer) {
-    return $this->handle->hasAutomaticCapability($capability, $viewer);
-  }
-
-}

src/applications/search/fulltextstorage/PhabricatorMySQLFulltextStorageEngine.php
@@ -184,7 +184,9 @@ final class PhabricatorMySQLFulltextStorageEngine
       $offset,
       $limit);
 
-    return ipull($results, 'documentPHID');
+    return id(new PhabricatorFulltextResultSet())
+      ->setPHIDs(ipull($results, 'documentPHID'))
+      ->setFulltextTokens($this->getFulltextTokens());
   }
 
   private function newFulltextSubquery(

src/applications/search/query/PhabricatorFulltextResultSet.php
@@ -3,6 +3,7 @@
 final class PhabricatorFulltextResultSet extends Phobject {
 
   private $phids;
+  private $fulltextHighlights;
   private $fulltextTokens;
 
   public function setPHIDs($phids) {
@@ -21,6 +22,22 @@ final class PhabricatorFulltextResultSet extends Phobject {
 
   public function getFulltextTokens() {
     return $this->fulltextTokens;
+  }
+
+  public function setFulltextHighlights($highlights) {
+    $this->fulltextHighlights = $highlights;
+    return $this;
+  }
+
+  public function getFulltextHighlights() {
+    return $this->fulltextHighlights;
+  }
+
+  public function getHighlightsForPHID($phid) {
+    if (!isset($this->fulltextHighlights[$phid])) {
+      return null;
+    }
+    return $this->fulltextHighlights[$phid];
   }
 
 }

src/applications/search/query/PhabricatorSearchApplicationSearchEngine.php
@@ -258,12 +258,21 @@ final class PhabricatorSearchApplicationSearchEngine
         ->withPHIDs(mpull($results, 'getPHID'))
         ->execute();
 
-      foreach ($results as $phid => $result) {
+      $ext = PhabricatorSearchResultEngineExtension::getAllEnabledExtensions();
+
+      foreach ($results as $phid => $handle) {
         $view = id(new PhabricatorSearchResultView())
-          ->setFulltextResult($result)
+          ->setHandle($handle)
           ->setQuery($query)
           ->setObject(idx($objects, $phid))
           ->render();
+
+        foreach($ext as $extension) {
+          $ext_view = $extension->renderItemView($result_set, $view, $phid);
+          if ($ext_view) {
+            $view = $ext_view;
+          }
+        }
         $list->addItem($view);
       }
     }

src/applications/search/query/PhabricatorSearchDocumentQuery.php
@@ -64,7 +64,7 @@ final class PhabricatorSearchDocumentQuery
     // Retain engine order.
     $handles = array_select_keys($handles, $phids);
 
-    return $results;
+    return $handles;
   }
 
   protected function willFilterPage(array $handles) {

src/applications/search/view/PhabricatorSearchResultView.php
@@ -12,12 +12,6 @@ final class PhabricatorSearchResultView extends AphrontView {
     return $this;
   }
 
-  public function setFulltextResult(PhabricatorFulltextResult $result) {
-    $this->result = $result;
-    $this->setHandle($result->getHandle());
-    return $this;
-  }
-
   public function setQuery(PhabricatorSavedQuery $query) {
     $this->query = $query;
     return $this;
@@ -52,13 +46,6 @@ final class PhabricatorSearchResultView extends AphrontView {
     if ($handle->getStatus() == PhabricatorObjectHandle::STATUS_CLOSED) {
       $item->setDisabled(true);
       $item->addAttribute(pht('Closed'));
-    }
-
-    $ext = PhabricatorSearchResultEngineExtension::getAllEnabledExtensions();
-    foreach($ext as $extension) {
-      if ($extension->canRenderItemView($this->result)) {
-        $item = $extension->renderItemView($item, $this->result);
-      }
     }
 
     return $item;

src/infrastructure/cluster/search/PhabricatorSearchService.php
@@ -259,11 +259,7 @@ class PhabricatorSearchService
 
       try {
         $engine = $service->getEngine();
-        $phids = $engine->executeSearch($query);
-
-        return id(new PhabricatorFulltextResultSet())
-          ->setPHIDs($phids)
-          ->setFulltextTokens($engine->getFulltextTokens());
+        return $engine->executeSearch($query);
       } catch (PhutilSearchQueryCompilerSyntaxException $ex) {
         // If there's a query compilation error, return it directly to the
         // user: they issued a query with bad syntax.